8499fa379398ad05ea48ef2a2d406a2fb036c1d1
MaMingming-1999 <67916069+MaMingming-1999@users.noreply.github.com>
2020-10-16 15:37:04 +0800

    前端-框架配置

---
+++ html/js/login.js
@@ -0,0 +1,9 @@
+function loginBtnStudent(){
+    window.location.href = "../student/index.html"
+}
+function loginBtnTeacher(){
+    window.location.href = "../teacher/index.html"
+}
+function loginBtnAdmin(){
+    window.location.href = "../teacherAdmin/index.html"
+}
